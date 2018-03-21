f227ee75e9718c600c64ddb42d8b8535199d9775
zhaolongtao <13295815771@163.com>
2018-03-21 18:37:18 +0800

    拍照可以上传啦

--- app/src/main/java/com/gongxianghui/fragments/locationFragment/LocationFragment.java
+++ app/src/main/java/com/gongxianghui/fragments/locationFragment/LocationFragment.java
@@ -1,15 +1,28 @@
 package com.gongxianghui.fragments.locationFragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.gongxianghui.R;
 import com.gongxianghui.base.BaseFragment;
+import com.gongxianghui.fragments.locationFragment.activity.PublishStateActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2018/3/9 0009.
  */
 
-public class LocationFragment extends BaseFragment{
+public class LocationFragment extends BaseFragment implements View.OnClickListener {
+    @BindView(R.id.tv_location_mine_fabu)
+    TextView tvLocationMineFabu;
+    Unbinder unbinder;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_location;
@@ -23,5 +36,33 @@ public class LocationFragment extends BaseFragment{
     @Override
     public void initViews(View view) {
 
+    }
+
+    @Override
+    protected void initListeners() {
+        tvLocationMineFabu.setOnClickListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_location_mine_fabu:
+                toActivity(PublishStateActivity.class);
+                break;
+        }
     }
 }
