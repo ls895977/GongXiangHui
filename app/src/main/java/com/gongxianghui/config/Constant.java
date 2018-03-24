eea2230f400e1f72df9b5006e4b83407e38d71cb
zhaolongtao <13295815771@163.com>
2018-03-24 14:03:31 +0800

    首页 便民介入网页成功 目前还没有进度条

--- app/src/main/java/com/gongxianghui/config/Constant.java
+++ app/src/main/java/com/gongxianghui/config/Constant.java
@@ -8,4 +8,29 @@ public class Constant {
     public static final long TIME_OUT = 3000;//网络请求超时时间
     public static final String URL="http://112.124.22.238:8081/course_api/wares/hot?pageSize=8&curPage=1";
 
+
+
+    //加载的网页链接
+    //常用电话
+    public static final String COMMON_PHONE = "http://app.zzbcar.com/zzb/helpcenter/protocol.html";
+    //查快递
+    public static final String CHECK_EXPRESS = "http://app.zzbcar.com/zzb/helpcenter/protocol.html";
+    //寄快递
+    public static final String SEND_EXPRESS = "http://app.zzbcar.com/zzb/helpcenter/user1.html";
+    //充话费
+    public static final String RUSH_PHONE_MONEY = "http://app.zzbcar.com/zzb/helpcenter/user2.html";
+    //公交查询
+    public static final String BUSS_CHECK = "http://app.zzbcar.com/zzb/helpcenter/user3.html";
+    //汽车票
+    public static final String CAR_TCKET = "http://app.zzbcar.com/zzb/helpcenter/user4.html";
+    //火车票
+    public static final String TRAIN_TICKET = "http://app.zzbcar.com/zzb/helpcenter/user5.html";
+    //飞机票
+    public static final String AIR_TICKET = "http://app.zzbcar.com/zzb/helpcenter/user6.html";
+    //农历黄历
+    public static final String ALMANAC = "http://app.zzbcar.com/zzb/helpcenter/user7.html";
+    //酒店住宿
+    public static final String HPTEL_ZHUSU = "http://app.zzbcar.com/zzb/helpcenter/user8.html";
+
+
 }
