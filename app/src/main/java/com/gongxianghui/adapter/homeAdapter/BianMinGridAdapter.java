aa9310e7de1b9194e5c32dd4ec5d398a0f864d00
zhaolongtao <13295815771@163.com>
2018-04-16 13:53:32 +0800

    暂存一下啊

--- app/src/main/java/com/gongxianghui/adapter/homeAdapter/BianMinGridAdapter.java
+++ app/src/main/java/com/gongxianghui/adapter/homeAdapter/BianMinGridAdapter.java
@@ -1,10 +1,34 @@
 package com.gongxianghui.adapter.homeAdapter;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
 import com.gongxianghui.adapter.baseAdapter.BaseRecycleViewAdapter;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/4/16 0016.
  */
 
 public class BianMinGridAdapter extends BaseRecycleViewAdapter {
+    public BianMinGridAdapter(Context context, List datas) {
+        super(context, datas);
+    }
+
+    @Override
+    protected void convert(MyViewHolder holder, int position, Object o) {
+
+    }
+
+    @Override
+    protected int getItemView() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
 }
