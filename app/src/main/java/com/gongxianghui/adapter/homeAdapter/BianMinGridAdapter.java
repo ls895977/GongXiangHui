f8c38e6da2ddc5ce10e3d1aa6dc57664857afe41
zhaolongtao <13295815771@163.com>
2018-04-16 13:53:13 +0800

    暂存下一啊  到时候会改动

---
+++ app/src/main/java/com/gongxianghui/adapter/homeAdapter/BianMinGridAdapter.java
@@ -0,0 +1,10 @@
+package com.gongxianghui.adapter.homeAdapter;
+
+import com.gongxianghui.adapter.baseAdapter.BaseRecycleViewAdapter;
+
+/**
+ * Created by Administrator on 2018/4/16 0016.
+ */
+
+public class BianMinGridAdapter extends BaseRecycleViewAdapter {
+}
