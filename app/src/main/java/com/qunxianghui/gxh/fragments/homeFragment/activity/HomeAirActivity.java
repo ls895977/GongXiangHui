4b8b21a71677870376da64339f5b48c2290ba671
zhaolongtao <13295815771@163.com>
2018-07-12 17:59:38 +0800

    暂存一下

--- app/src/main/java/com/qunxianghui/gxh/fragments/homeFragment/activity/HomeAirActivity.java
+++ app/src/main/java/com/qunxianghui/gxh/fragments/homeFragment/activity/HomeAirActivity.java
@@ -3,7 +3,6 @@ package com.qunxianghui.gxh.fragments.homeFragment.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
