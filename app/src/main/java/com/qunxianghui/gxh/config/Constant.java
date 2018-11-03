e630ffb32e9a159cc769c025c4d30eed61f346e4
zhaolongtao <13295815771@163.com>
2018-11-03 10:04:16 +0800

    准备处理 定位切换信息的问题

--- app/src/main/java/com/qunxianghui/gxh/config/Constant.java
+++ app/src/main/java/com/qunxianghui/gxh/config/Constant.java
@@ -43,7 +43,7 @@ public class Constant {
 
     public static final String HOME_LOCAL_SERVICE_URL = "http://api.test.gongxianghui.net/theme/new_details/bendi_h5/index.html#/";  //首页上面本地服务的页面
 
-//    /*线上环境的详情页*/
+    /*线上环境的详情页*/
 //    public static final String HOME_NEWS_DETAIL_URL = "https://api.qunxianghui.com.cn/theme/new_details/xinwen_/index.html#/";    //新闻详情
 //
 //    public static final String LOCAL_SERVICES_ISSUE_URL = "https://api.qunxianghui.com.cn/theme/new_details/bendi_fabu/index.html#/";  //本地发布详情
